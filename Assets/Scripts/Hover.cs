398a9747cb8a8d9bec33beee1975153880ca97d4
LttlGreenSprout <2013cmercado@gmail.com>
2014-12-17 17:34:38 -0500

    Added Hover script to the stairs. It makes things oscillate slowly up and down.

---
+++ Assets/Scripts/Hover.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hover : MonoBehaviour {
+
+    //Oscillation height
+    public float amplitude = .1f;
+    //Oscillations per second
+    public float frequency = 1f;
+
+    //Stores the original height
+    private float initY;
+    private float initTimeOffset;
+
+	// Use this for initialization
+	void Start () {
+        initY = gameObject.transform.position.y;
+        initTimeOffset = Random.Range(0.0f, 1f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 newPos = new Vector3(gameObject.transform.position.x, initY + amplitude * Mathf.Sin(initTimeOffset + frequency * Time.unscaledTime), gameObject.transform.position.z);
+        gameObject.transform.position = newPos;
+	}
+
+    void OnCollisionEnter(Collision c)
+    {
+        Debug.Log("collsion");
+    }
+
+    void OnTriggerEnter()
+    {
+        Debug.Log("trigger");
+    }
+}
